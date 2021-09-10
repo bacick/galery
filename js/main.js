cbf98910b8230d9af2fb33ca3410eddf2ca3b359
bacick <eshtin@gmail.com>
2021-09-10 19:53:31 +0300

    create poject, instal node

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let images = [
+    { img: '1.jpg', name: 'Мельница у реки' },
+    { img: '2.jpg', name: 'Горы осенью' },
+    { img: '3.jpg', name: 'Деревня летом' },
+    { img: '4.gif', name: 'Котэ' },
+    { img: '1.mp3', name: 'Птички чипчихуйки' },
+    { img: '2.mp3', name: 'Ручей' },
+    { img: '3.mp3', name: 'Звуки леса' },
+    { img: '1.mp4', name: 'Never Gonna Give You Up' },
+    { img: '2.mp4', name: 'Морской прибой' },
+];
+let regexpimg = /\.(png|jpg?g|gif)$/;
+let regexaudio = /\.mp3$/;
+let content = document.querySelector('.content');
+console.log(content);
+images.forEach(el => {
+    if (regexpimg.test(el.img)) {
+        content.insertAdjacentHTML('beforeend',
+            `<div class="block"><img class="media" src='../media/images/${el.img}'><p>${el.name}</p></div>`)
+    } else if(regexaudio.test(el.img)){
+        content.insertAdjacentHTML('beforeend',
+            `<div class="block"><audio class="media" controls><source src='../media/audio/${el.img}'></audio><p>${el.name}</p></div>`);
+    } else {
+        content.insertAdjacentHTML('beforeend',
+            `<div class="block"><video class="media" controls><source src='../media/video/${el.img}'></video><p>${el.name}</p></div>`);
+    }
+    });
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const { resolve } = require('path');
+const cssExtractPlugin = require('mini-css-extract-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './js/main.js',
+    output: {
+        path: resolve(__dirname, 'build'),
+        filename: 'main.[contenthash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg?g|mp3)$/i,
+                use: [
+                    {
+                      loader: 'file-loader',
+                      options: {
+                        name: '[path][name].[ext]',
+                      },
+                    },
+                  ],
+        },
+            {
+                test: /\\.css$/,
+                use: [cssExtractPlugin.loader, 'css-loader']
+
+        },
+            {
+                test: /\.(sa|sc|c)ss$/i,
+                use: [cssExtractPlugin.loader,'css-loader','sass-loader']
+        }]
+    },
+    plugins: [
+        new cssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+        new htmlWebpackPlugin({
+            template: resolve(__dirname,'index.html')
+        })
+    ]
+}
